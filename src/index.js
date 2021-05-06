721e4647eb945a5a6eb11e28f633e3bfdedca9ef
garciajordy <jordygarcia0119@gmail.com>
2021-05-06 13:12:06 +0300

    Add logic for switching tabs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
+console.log('Hello World!');
 import { compareAsc, format } from 'date-fns'
-
+import "../style/style.css"
 const date = format(new Date(2014, 1, 11), 'yyyy-MM-dd')
 //=> '2014-02-11'
-
+let content =document.getElementById("content")
 const dates = [
   new Date(1995, 6, 2),
   new Date(1987, 1, 11),
@@ -20,8 +21,47 @@ dates.sort(compareAsc)
 const Task = require('./task').default;
 const Project = require('./project').default;
 
+let projectArray = []
+function addProjectToArray(name) {
+  projectArray.push(new Project(name))
+}
 
 let task1 = new Task('Go Fishing', 'Going finshing', date);
-let proj1 = new Project('New Task');
+addProjectToArray("New Task")
+addProjectToArray("second Task")
+addProjectToArray("third Task")
+addProjectToArray("fourth Task")
+addProjectToArray("five Task")
+let proj1 = projectArray[0]
 proj1.array.push(task1);
 console.log(proj1);
+let ul = document.createElement("ul")
+
+function displayToDo() {
+  let array = projectArray[this.id].array
+  for (let i = 0; i < array.length; i++){
+    let p = document.createElement("p")
+    p.innerHTML = array[i].name
+    content.appendChild(p)
+
+  }
+}
+for (let i = 0; i < projectArray.length; i++){
+  let li = document.createElement("li")
+  let a = document.createElement("a")
+  a.id = i
+  a.href = "#"
+  a.innerHTML = projectArray[i].name
+  a.addEventListener("click", displayToDo)
+  li.appendChild(a)
+  ul.appendChild(li)
+}
+content.appendChild(ul)
+
+  
+{/* <ul>
+<li><a href="#home">Home</a></li>
+<li><a href="#news">News</a></li>
+<li><a href="#contact">Contact</a></li>
+<li><a href="#about">About</a></li>
+</ul> */}
