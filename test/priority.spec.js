d3447ade181d0750e3f317edc271329c970ecccd
garciajordy <jordygarcia0119@gmail.com>
2021-05-10 13:30:30 +0300

    Add test for update function

--- test/priority.spec.js
+++ test/priority.spec.js
@@ -1,7 +1,7 @@
 const Task = require('../src/task');
 const priority = require('../src/priority');
 
-test('create task', () => {
+test('test the priority function', () => {
   const testTask = new Task('test', 'description-test', '05-05-2020');
   expect(testTask.name).toBe('test');
   expect(testTask.description).toBe('description-test');
---
+++ test/update.spec.js
@@ -0,0 +1,16 @@
+const Task = require('../src/task');
+const update = require('../src/update');
+
+test('test the update function', () => {
+  const testTask = new Task('test', 'description-test', '05-05-2020');
+  let h5 = document.createElement("h5")
+    let h6 = document.createElement("h6")
+    let p = document.createElement("p")
+    h5.innerHTML = "updated-test"
+    p.innerHTML = "updated-description"
+    h6.innerHTML = "06-06-2020"
+  update(testTask, h5,h6,p);
+    expect(testTask.name).toBe("updated-test");
+    expect(testTask.date).toBe("06-06-2020");
+    expect(testTask.description).toBe("updated-description");
+});
